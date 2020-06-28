f1d64ba4e419b517e46fbccb319a11c0279c50cf
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2020-06-28 00:19:45 -0500

    Removed extra includes

--- CheckAFK.py
+++ CheckAFK.py
@@ -1,6 +1,4 @@
-import requests
 import pandas
-from bs4 import BeautifulSoup as bs
 from pushsafer import init, Client
 from Utilities import getActiveCodes
 import numpy as np
@@ -18,18 +16,17 @@ init(key.read())
 if newcodedf.equals(oldcodedf):
     Client("").send_message('No new codes today', "AFK Code drop", "26131", "37", "0", "2", "https://afk.guide/redemption-codes/", "Go to codes", "0", "2", "60", "600", "1", "", "", "")
 else:
-    #Covert data frames to 2d lists for iteration
+    # Convert data frames to 2d lists for iteration
     newdata = np.vstack((newcodedf['Codes'].to_list(), newcodedf['Rewards'].to_list()))
     olddata = np.vstack((oldcodedf['Codes'].to_list(), oldcodedf['Rewards'].to_list()))
 
     # Check for new codes, ignore if one expired
     for i in range(0, len(newdata[0])):
-        print(newdata[0][i], newdata[1][i])
         if newdata[0][i] not in olddata[0]:
             # Build notification message
             message = 'New Code: "' + str(newdata[0][i]) + '" for ' + str(newdata[1][i]) 
            
-            # Send notification
+            # Send notification with format:
             # client.send_message("Message", "Title", "Device or Device Group ID", "Icon", "Sound", "Vibration", "URL", "URL Title", "Time2Live", "Priority", "Retry", "Expire", "Answer", "Image 1", "Image 2", "Image 3")
             Client("").send_message(message, "AFK Code Drop", "26131", "37", "0", "2", "https://afk.guide/redemption-codes/", "Go to codes", "0", "2", "60", "600", "1", "", "", "")
 
