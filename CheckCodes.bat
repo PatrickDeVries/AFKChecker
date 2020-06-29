3a78773c210c7dfbe548cd1c8eb176abd35faa29
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2020-06-29 09:47:33 -0500

    Fixed BAT

--- CheckCodes.bat
+++ CheckCodes.bat
@@ -1,4 +1,4 @@
 REM Use Task Scheduler to automatically run this bat file to run the python script
 REM "Path to python.exe" "Path to CheckAFK.py"
-"python" "CheckAFK.py"
+"python" "C:\Users\green\pyprojs\AFKChecker\CheckAFK.py"
 pause
