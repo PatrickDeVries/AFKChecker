e32a37d3449ab30516b26483a580e0e7bf58c663
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2020-09-28 11:50:26 -0500

    fixed image alt text replacement issue

--- Utilities.py
+++ Utilities.py
@@ -14,6 +14,8 @@ def getActiveCodes():
     table = table.find('tbody')
     rows = table.find_all('tr')
     imgs = table.find_all('img')
+    
+    print("rows = ", rows)
 
     #Get list of alt texts for images
     alts = [img['alt'] for img in table.find_all('img', alt=True)]
@@ -23,18 +25,20 @@ def getActiveCodes():
     for tr in rows:
         td = tr.find_all('td')
         row = [i.get_text() for i in td]
+        print("row = ", row)
         # Remove extra text from codes and turn rewards into a list split by spaces
-        row = [row[0].split(" ")[0], row[-1].replace(u'\xa0', u' ').split(' ')]
-        
+        row = [row[0].split(" ")[0], row[1].replace(u'\xa0', u' ').split(' ')]
+        print("after split = ", row)
         # Add image alts
-        for i in range(0, len(row[-1])):
-            alt = alts[altused]
-            altused+=1
-            row[-1][i] = row[-1][i] + ' ' + alt
-
+        if alts:
+            for i in range(0, len(row[-1])):
+                alt = alts[altused]
+                altused+=1
+                row[-1][i] = row[-1][i] + ' ' + alt
         # Join rewards list into one string
-        row[-1] = ', '.join(row[-1])
-        
+        row[1] = ' '.join(row[1])
+        print("after join = ", row)
+
         codes.append(row)
     
     # Convert codes to a dataframe
@@ -58,4 +62,5 @@ def sendNotif(subject, message, sender, destination):
     msg['To'] = destination
     msg.attach(MIMEText(message))
     server.sendmail(sender, destination, msg.as_string())
+    print("sent mail")
     server.quit()
