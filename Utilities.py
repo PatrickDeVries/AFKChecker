f95b642837e717cfdcd27abb15decc22fb0e389b
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2020-09-28 11:54:52 -0500

    backwards compatible in case site changes back to images

--- Utilities.py
+++ Utilities.py
@@ -15,8 +15,6 @@ def getActiveCodes():
     rows = table.find_all('tr')
     imgs = table.find_all('img')
     
-    print("rows = ", rows)
-
     #Get list of alt texts for images
     alts = [img['alt'] for img in table.find_all('img', alt=True)]
     altused = 0
@@ -25,19 +23,23 @@ def getActiveCodes():
     for tr in rows:
         td = tr.find_all('td')
         row = [i.get_text() for i in td]
-        print("row = ", row)
-        # Remove extra text from codes and turn rewards into a list split by spaces
-        row = [row[0].split(" ")[0], row[1].replace(u'\xa0', u' ').split(' ')]
-        print("after split = ", row)
-        # Add image alts
-        if alts:
+        
+        if alts: 
+            # Remove extra text from codes and turn rewards into a list split by spaces
+            row = [row[0].split(" ")[0], row[-1].replace(u'\xa0', u' ').split(' ')]
+            # Add image alts
             for i in range(0, len(row[-1])):
                 alt = alts[altused]
                 altused+=1
                 row[-1][i] = row[-1][i] + ' ' + alt
-        # Join rewards list into one string
-        row[1] = ' '.join(row[1])
-        print("after join = ", row)
+            # Join rewards list into one string
+            row[-1] = ' '.join(row[-1])
+        else:
+            # Remove extra text from codes and turn rewards into a list split by spaces
+            row = [row[0].split(" ")[0], row[1].replace(u'\xa0', u' ').split(' ')]
+            # Join rewards list into one string
+            row[1] = ' '.join(row[1])
+
 
         codes.append(row)
     
