f214d97781bad198ea792c388558af9007768181
Patrick DeVries <pdevries@smu.edu>
2020-11-19 19:48:14 -0600

    fixed fetching of codes to reflect website formatting update

--- Utilities.py
+++ Utilities.py
@@ -15,34 +15,15 @@ def getActiveCodes():
     rows = table.find_all('tr')
     imgs = table.find_all('img')
     
-    #Get list of alt texts for images
-    alts = [img['alt'] for img in table.find_all('img', alt=True)]
-    altused = 0
+    div = soup.find('div', {'data-td-block-uid':'tdi_48_566'})
+    lst = div.find('ul')    # for i in lst:
+
     codes = []
+    for li in lst.find_all('li'):
+        print("{0}: ".format(li.text))
+        item = li.text.split('–')
+        codes.append((item[0], item[1]))
 
-    for tr in rows:
-        td = tr.find_all('td')
-        row = [i.get_text() for i in td]
-        
-        if alts: 
-            # Remove extra text from codes and turn rewards into a list split by spaces
-            row = [row[0].split(" ")[0], row[-1].replace(u'\xa0', u' ').split(' ')]
-            # Add image alts
-            for i in range(0, len(row[-1])):
-                alt = alts[altused]
-                altused+=1
-                row[-1][i] = row[-1][i] + ' ' + alt
-            # Join rewards list into one string
-            row[-1] = ' '.join(row[-1])
-        else:
-            # Remove extra text from codes and turn rewards into a list split by spaces
-            row = [row[0].split(" ")[0], row[1].replace(u'\xa0', u' ').split(' ')]
-            # Join rewards list into one string
-            row[1] = ' '.join(row[1])
-
-
-        codes.append(row)
-    
     # Convert codes to a dataframe
     codedf = pandas.DataFrame(codes)
     codedf.columns = ['Codes', 'Rewards']
